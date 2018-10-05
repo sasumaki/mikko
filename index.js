8154ee812d53b78dbc9ee6605dce332e849d1782
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-10-05 13:33:49 +0300

    access db

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
 'use strict'
 
 const program = require('commander'),
-      exec = require('child_process').exec;
+  exec = require('child_process').exec;
 
-
+let execCallback = (error, stdout, stderr) => {
+  if (error) console.log("exec error: " + error);
+  if (stdout) console.log("Result: " + stdout);
+  if (stderr) console.log("shell error: " + stderr);
+}
 const updateProduction = (container, options) => {
   let cmd = ''
-  if (options.iso) cmd += 'nohup docker exec ' 
+  if (options.iso) cmd += 'nohup docker exec '
   if (options.production) cmd += container
   cmd += ' bash -c "npm run update_database file=/data/all_student_numbers.txt"'
   if (options.iso) cmd += ' &'
 
-  let execCallback = (error, stdout, stderr) => {
-    if (error) console.log("exec error: " + error);
-    if (stdout) console.log("Result: " + stdout);
-    if (stderr) console.log("shell error: " + stderr);
-  };
+
   exec(cmd, execCallback);
 }
+const opendb = (container) =>  exec(`docker exec -it -u postgres ${container} psql -d tkt_oodi`, execCallback)
+
 
 program
   .version('0.0.1')
@@ -26,5 +28,8 @@ program
   .description('run database updater')
   .option('-i', '--iso', 'flag to run in nohup')
   .action(updateProduction)
+  .command('db [container]')
+  .description(' open database psql in containers')
+  .action(opendb)
 
 program.parse(process.argv);
