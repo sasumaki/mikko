db62eb7efaa88a2a639be36f73d97649e519cc7b
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-10-05 14:18:46 +0300

    minthified for now

--- index.js
+++ index.js
@@ -11,24 +11,30 @@ let execCallback = (error, stdout, stderr) => {
   if (stdout) console.log("Result: " + stdout);
   if (stderr) console.log("shell error: " + stderr);
 }
-const updateProduction = (container, options) => {
-  let cmd = ''
-  if (options.iso) cmd += 'nohup docker exec '
-  if (options.production) cmd += container
-  cmd += ' bash -c "npm run update_database file=/data/all_student_numbers.txt"'
-  if (options.iso) cmd += ' &'
-
+const updateProduction = (container) => {
+  const target = container === 'prod' ? 'backend' : 'staging_backend'
+  let cmd = `nohup docker exec ${target} bash -c "npm run update_database file=/data/all_student_numbers.txt"  &`
 
   exec(cmd, execCallback);
 }
-const opendb = (container) =>  spawn('docker', ['exec', '-it', '-u', 'postgres', container, 'psql', '-d', 'tkt_oodi'], { stdio: 'inherit' })
+const opendb = (container) => {
+  switch (container) {
+    case 'prod':
+      spawn('docker', ['exec', '-it', '-u', 'postgres', 'db', 'psql', '-d', 'tkt_oodi'], { stdio: 'inherit' })
+    case 'staging':
+      spawn('docker', ['exec', '-it', '-u', 'postgres', 'db_staging', 'psql', '-d', 'tkt_oodi_staging'], { stdio: 'inherit' })
+    case 'testing':
+      spawn('docker', ['exec', '-it', '-u', 'postgres', 'db_testing', 'psql', '-d', 'tkt_oodi_testing'], { stdio: 'inherit' })
+    default:
+      spawn('docker', ['exec', '-it', '-u', 'postgres', 'oodi_db', 'psql', '-d', 'tkt_oodi'], { stdio: 'inherit' })
+  }
+}
 
 
 program
   .version(version)
   .command('update [container]')
   .description('run database updater')
-  .option('-i', '--iso', 'flag to run in nohup')
   .action(updateProduction)
 
 program
